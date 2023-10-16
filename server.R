2e8b7846179b98c801a4fd633afe92e1ed5dca3f
muhlenlogan8 <muhlenlogan8@gmail.com>
2023-10-15 21:14:08 -0400

    Removing comment, testing if everything works after repo name change

--- server.R
+++ server.R
@@ -4,13 +4,6 @@ source("InputExcelFile.R")
 
 options(shiny.maxRequestSize = 30*1024^2)
 
-# server <- function(input, output, session) {
-#   output$mychoice <- renderText(
-#     input$select
-#   )
-#   CreateTable(input, output, session)
-# }
-
 server <- function(input, output, session) {
   output$mychoice <- renderText(input$select)
   observeEvent(
